a61793a77c4869c31e831483d1336653191f0d91
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-10-31 15:46:14 -0500

    first commit

---
+++ script.rb
@@ -0,0 +1,21 @@
+num = 1
+for num in  1..20
+    if num % 2 == 0
+        puts "#{num} is an EVEN number"
+    else 
+        puts "#{num} is an ODD number"
+    end 
+end 
+
+
+
+num2 = 0
+
+while num2 < 20
+    num2 = num2 + 1 
+    if num2 % 2 == 0
+        puts "#{num2} is an EVEN number"
+    else 
+        puts "#{num2} is an ODD number"
+    end 
+end 
